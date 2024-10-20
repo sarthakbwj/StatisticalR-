137b76fe7b7aa35ebdfb1a948a38fdfb39a1ca78
sarthakbwj <156067427+sarthakbwj@users.noreply.github.com>
2024-10-20 19:39:34 +0530

    Update Representing_Data_L1.R

--- Lectures/Representing_Data_L1.R
+++ Lectures/Representing_Data_L1.R
@@ -2,8 +2,27 @@
 # Vector(Data type) is a one dimentional list of data. 
 # 2 vectors form a dataframe. 
 
+# c() "combines" individual data of same type into a string giving it a structure
+# hence it is called a Data Structure/vector.
+words <- c("It's", "a", "beautiful", "day.")
+words
+
+# repeats the vector for number of times 
+rep(words, time = 2)
+
+# 
+numbers <- c(1:10)
+numbers
+
+
+
+
+
+# DATAFRAMES
+
 spacecraft <- data.frame(
-  row.names = c("Voyager1", "Voyager2", "Voyager10"),  #character string vector
+  names = c("Voyager1", "Voyager2", "Pioneer10"),  #character string vector
+  # row.names = c("Voyager1", "Voyager2", "Pioneer10"),
   distance = c(163,136,80)  #numeric vector
 ) 
 # 2 Data types cannot exist together in one vector, they can exist separately in distinct vectors. 
