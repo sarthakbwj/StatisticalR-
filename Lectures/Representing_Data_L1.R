d6784616d9819ecd3c3bc6b0a52f1ba354002cc5
sarthakbwj <156067427+sarthakbwj@users.noreply.github.com>
2024-10-20 17:08:18 +0530

    L1
    L1

--- Lectures/Representing_Data_L1.R
+++ Lectures/Representing_Data_L1.R
@@ -1,1 +1,15 @@
-print("Hello World!")
+# Dataframes are tables to store data in rows and columns.
+# Vector(Data type) is a one dimentional list of data. 
+# 2 vectors form a dataframe. 
+
+spacecraft <- data.frame(
+  name = c("Voyager1", "Voyager2", "Voyager10"),  #character string vector
+  distance = c(163,136,80)  #numeric vector
+) 
+# 2 Data types cannot exist together in one vector, they can exist separately in distinct vectors. 
+
+spacecraft 
+
+# $ + name of the column outputs the column
+spacecraft$name
+
