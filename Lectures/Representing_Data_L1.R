03cb380404bde06729843f5f13fd209f45b24dd2
sarthakbwj <156067427+sarthakbwj@users.noreply.github.com>
2024-10-20 17:43:33 +0530

    Update Representing_Data_L1.R

--- Lectures/Representing_Data_L1.R
+++ Lectures/Representing_Data_L1.R
@@ -3,7 +3,7 @@
 # 2 vectors form a dataframe. 
 
 spacecraft <- data.frame(
-  name = c("Voyager1", "Voyager2", "Voyager10"),  #character string vector
+  row.names = c("Voyager1", "Voyager2", "Voyager10"),  #character string vector
   distance = c(163,136,80)  #numeric vector
 ) 
 # 2 Data types cannot exist together in one vector, they can exist separately in distinct vectors. 
@@ -13,3 +13,17 @@ spacecraft
 # $ + name of the column outputs the column
 spacecraft$name
 
+# outputs 1st column, subset of the dataframe. 
+spacecraft[1]
+
+# outputs 1st column as a vector(list)
+spacecraft[[1]]
+
+# outputs the desired index.[row,column]
+spacecraft[1,2]
+
+# outputs the column of the index
+spacecraft[, 1]
+
+# outputs the row of the index
+spacecraft[1,]
