8f29eacb089b838ffb13d8a0a8b777a7baf409ce
sarthakbwj <156067427+sarthakbwj@users.noreply.github.com>
2024-10-20 20:47:51 +0530

    Update Representing_Data_L1.R

--- Lectures/Representing_Data_L1.R
+++ Lectures/Representing_Data_L1.R
@@ -1,7 +1,4 @@
-# Dataframes are tables to store data in rows and columns.
-# Vector(Data type) is a one dimentional list of data. 
-# 2 vectors form a dataframe. 
-
+print("Hello World!")
 # c() "combines" individual data of same type into a string giving it a structure
 # hence it is called a Data Structure/vector.
 words <- c("It's", "a", "beautiful", "day.")
@@ -10,15 +7,26 @@ words
 # repeats the vector for number of times 
 rep(words, time = 2)
 
-# 
-numbers <- c(1:10)
+# outputs range of numbers
+numbers <- 1:10
 numbers
+
+# sequence 
+num <- seq(from = 1, to = 10, by = 2)
+num
+
+
 
 
 
 
 
 # DATAFRAMES
+
+
+# Dataframes are tables to store data in rows and columns.
+# Vector(Data type) is a one dimentional list of data. 
+# 2 vectors form a dataframe. 
 
 spacecraft <- data.frame(
   names = c("Voyager1", "Voyager2", "Pioneer10"),  #character string vector
@@ -46,3 +54,33 @@ spacecraft[, 1]
 
 # outputs the row of the index
 spacecraft[1,]
+
+
+
+
+
+# To get user input - "readline"
+
+name <- readline("What's your name? ")
+# using "paste" to concatenate strings 
+# paste0 function concatenates 2 strings without any spaces
+greeting <- paste("Hello, ", name, sep = "")
+print(greeting)
+
+
+
+
+#count.R
+
+Mario <- as.integer(readline("Enter votes for Mario: "))
+Peach <- as.integer(readline("Enter votes for Peach: "))
+Bowser <- as.integer(readline("Enter votes for Bowser: "))
+
+Total <- sum(Mario , Peach , Bowser)
+
+print(paste("Total Votes:", Total))
+
+
+
+
+
