b6ac15da1e489e6e536ab6d126c1ec7e5d9dfe01
sarthakbwj <156067427+sarthakbwj@users.noreply.github.com>
2024-10-23 17:58:42 +0530

    Update Transforming_Data_L2.R

--- Lectures/Transforming_Data_L2/Transforming_Data_L2.R
+++ Lectures/Transforming_Data_L2/Transforming_Data_L2.R
@@ -16,12 +16,22 @@ temps[-c(2,4,7)]
 # Logical Expressions is a 'comparison operator' that give us an output in TRUE or FALSE (T/F). 
 # similar to bools in Python. 
 
-
-temp[2]
-
-
-
+  
+# outputs indexes which return TRUE as the result of the evaluating each expression.
+which(temps < 0) 
 
 
+# 'Logical operators' allows the user to combine 2 or more expressing and ask complex questions.
+# & - 'and'
+# | - 'or'
 
+which(temps < 0 | temps > 60)
+
+# 'any' logical operator = if any data point evaluated results to True, the output is TRUE and vice versa. 
+# are any of these values outliers? 
+any(temps < 0 | temps > 60)
+ 
+# 'all' logical operator = if all data points evaluated result to True, the output is TRUE and vice versa. 
+# are all of these values outliers? 
+all(temps < 0 | temps > 60)
 
