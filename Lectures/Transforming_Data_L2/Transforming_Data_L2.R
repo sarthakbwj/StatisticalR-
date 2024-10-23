b23742b1893ee1cbbc59a638fd793d165adf5c85
sarthakbwj <156067427+sarthakbwj@users.noreply.github.com>
2024-10-23 12:38:17 +0530

    Update Transforming_Data_L2.R

--- Lectures/Transforming_Data_L2/Transforming_Data_L2.R
+++ Lectures/Transforming_Data_L2/Transforming_Data_L2.R
@@ -1,1 +1,27 @@
 # Outliers are data points which fall outside of the Data range. 
+
+load("temps.RData") # load Data in IDE
+mean(temps) 
+
+# index into the data using position values
+temps[2] 
+temps[4]
+temps[7]
+
+# creating a vector(list) of outliers and removing them from
+# the original vector - Transforming Vectors
+temps[-c(2,4,7)]
+
+
+# Logical Expressions is a 'comparison operator' that give us an output in TRUE or FALSE (T/F). 
+# similar to bools in Python. 
+
+
+temp[2]
+
+
+
+
+
+
+
