f9f657122b2f612d9c288faa6fdc0890911f1dfc
sarthakbwj <156067427+sarthakbwj@users.noreply.github.com>
2024-11-06 09:52:47 +0530

    Update count.R

--- Lectures/Applying_Functions_L3/count.R
+++ Lectures/Applying_Functions_L3/count.R
@@ -1,5 +1,10 @@
 get_votes <- function(prompt = "Enter Votes: ") {    # create function and parameter
-  votes <- as.integer(readline(prompt))       # deploy parameter
+  votes <- as.integer(readline(prompt))     # deploy parameter
+  if (is.na(votes)) {
+    return (0)
+  } else {
+    return (votes)
+  }
 }
 
 Mario <- get_votes("Mario: ")     # define parameter 
@@ -13,3 +18,4 @@ print(paste("Total Votes:", Total))
 # SCOPE: 
 # certain functions have a global scope while the one's defined within the 
 # function body have a local scope. 
+
