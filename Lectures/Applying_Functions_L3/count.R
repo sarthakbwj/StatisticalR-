c43bbf8665acbfefbb1b6bd9f42671c7fcf9dd82
sarthakbwj <156067427+sarthakbwj@users.noreply.github.com>
2024-11-06 09:47:29 +0530

    Update count.R

---
+++ Lectures/Applying_Functions_L3/count.R
@@ -0,0 +1,15 @@
+get_votes <- function(prompt = "Enter Votes: ") {    # create function and parameter
+  votes <- as.integer(readline(prompt))       # deploy parameter
+}
+
+Mario <- get_votes("Mario: ")     # define parameter 
+Peach <- get_votes("Peach: ")
+Bowser <- get_votes("Bowser: ")
+
+Total <- sum(Mario , Peach , Bowser)
+print(paste("Total Votes:", Total))
+
+
+# SCOPE: 
+# certain functions have a global scope while the one's defined within the 
+# function body have a local scope. 
