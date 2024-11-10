61fcb95c1b3c6facd6750186be9793a26dd2389b
sarthakbwj <156067427+sarthakbwj@users.noreply.github.com>
2024-11-10 15:59:01 +0530

    Update tabulate.R

--- Lectures/Applying_Functions_L3/tabulate.R
+++ Lectures/Applying_Functions_L3/tabulate.R
@@ -1,7 +1,15 @@
 votes <- read.csv("/Users/sarthakbwj/Documents/GitHub/Statistical_R/Lectures/Applying_Functions_L3/votes.csv")
+View(votes)
 
 total_votes <- c() # create a vector
 for (candidate in rownames(votes)){ # for loop to traverse through each row of the data set.
   total_votes[candidate] <-  sum(votes[candidate, ]) # storing the total votes for a candidate along with the candidate name. 
 }
 total_votes
+
+
+# APPLY FUNCTION 
+# apply helps iterate over rows and columns and perform operations, no need to write loops.  
+# margin 1 = rows and margin 2 = columns. 
+
+apply(votes, MARGIN = 1, FUN = sum)
