b2c45510c392c2cf9b50ea5ee8ba984a353e9cdd
sarthakbwj <156067427+sarthakbwj@users.noreply.github.com>
2024-11-10 15:51:02 +0530

    Update tabulate.R

--- Lectures/Applying_Functions_L3/tabulate.R
+++ Lectures/Applying_Functions_L3/tabulate.R
@@ -1,6 +1,7 @@
 votes <- read.csv("/Users/sarthakbwj/Documents/GitHub/Statistical_R/Lectures/Applying_Functions_L3/votes.csv")
-View(votes)
-  
-for (candidates in rowname(votes)){
-  
+
+total_votes <- c() # create a vector
+for (candidate in rownames(votes)){ # for loop to traverse through each row of the data set.
+  total_votes[candidate] <-  sum(votes[candidate, ]) # storing the total votes for a candidate along with the candidate name. 
 }
+total_votes
