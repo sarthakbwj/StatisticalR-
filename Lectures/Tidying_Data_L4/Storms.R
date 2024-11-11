b3ecb564932426b773788e9fcdb728793bb8a993
sarthakbwj <156067427+sarthakbwj@users.noreply.github.com>
2024-11-11 23:22:12 +0530

    Update Storms.R

--- Lectures/Tidying_Data_L4/Storms.R
+++ Lectures/Tidying_Data_L4/Storms.R
@@ -1,9 +1,11 @@
 storm
 
+
 # load a package and use a function from it. 
 
 dplyr::select(
   storms, 
-  !c(lat, pressure, tropicalstorm_force_diameter, hurricane_force_diameter)
+  !c(lat, pressure, endsWith("diameter"))
 )
 
+  
