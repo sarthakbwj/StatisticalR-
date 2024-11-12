6b4207b4e1eaf1f183877cea0a63ea4ef5755d6c
sarthakbwj <156067427+sarthakbwj@users.noreply.github.com>
2024-11-12 20:20:09 +0530

    Update Storms.R

--- Lectures/Tidying_Data_L4/Storms.R
+++ Lectures/Tidying_Data_L4/Storms.R
@@ -2,6 +2,11 @@ storms
 # CRAN : Comprehensive R Archive Network 
 # load a package and use a function from it. 
 
+# chr = character
+# dbl = double (similar to floating value in python)
+# fct = factors (categories which can be grouped)
+
+
 dplyr::select(
   storms, 
   !c(lat, pressure, endsWith("diameter"))
@@ -17,9 +22,16 @@ dplyr::filter(
 )
 
 
-# pipe operator 
+# PIPE OPERATOR (|>)
+# allows the first argument(name of Data Set) to be written outside and continued 
+# by the other arguments. Its similar to nesting in python. The output of one is the
+# input for another. 
 
-storms |> select 
+storms |> 
+  select(!c(lat, long, pressure, ends_with("diameter"))) |>
+  filter(status == "hurricane")
+  
+
 
 
 
