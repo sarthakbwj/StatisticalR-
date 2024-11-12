670e230d3acf23d08e70e5f49335178d2f491570
sarthakbwj <156067427+sarthakbwj@users.noreply.github.com>
2024-11-12 21:39:51 +0530

    hurricane.csv

---
+++ Lectures/Tidying_Data_L4/hurricane.R
@@ -0,0 +1,3 @@
+hurricane <- read.csv("/Users/sarthakbwj/Documents/GitHub/Statistical_R/Lectures/Tidying_Data_L4/hurricanes.csv")
+View(hurricane)
+# sort as per the strongest hurricane from the DataSet. 
