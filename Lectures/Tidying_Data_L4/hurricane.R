f76fb6a709254bb92745baae59d35638a3c892a7
sarthakbwj <156067427+sarthakbwj@users.noreply.github.com>
2024-11-12 22:05:34 +0530

    Update hurricane.R

--- Lectures/Tidying_Data_L4/hurricane.R
+++ Lectures/Tidying_Data_L4/hurricane.R
@@ -12,3 +12,11 @@ hurricane |>
   arrange(desc(wind)) |>
   slice_head()  # output the first row of the groups. 
 
+
+# slice_head - returns the 1st row of the group.
+# slice_tail - returns the last row of the group.
+# slice_max - returns the maximum value from the group.
+# slice_min - returns the minimum value from the group. 
+
+
+
