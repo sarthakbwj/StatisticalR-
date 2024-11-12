45fe8f0bda670ae4056e3bc5b086dc606b045038
sarthakbwj <156067427+sarthakbwj@users.noreply.github.com>
2024-11-12 20:54:57 +0530

    Update Storms.R

--- Lectures/Tidying_Data_L4/Storms.R
+++ Lectures/Tidying_Data_L4/Storms.R
@@ -9,30 +9,37 @@ storms
 
 dplyr::select(
   storms, 
-  !c(lat, pressure, endsWith("diameter"))
+  !c(lat, pressure, ends_with("diameter"))
 )
 
   
 dplyr::filter(
   dplyr::select(
     storms,
-    !c(lat, pressure, endsWith("diameter"))
+    !c(lat, pressure, ends_with("diameter"))
   ),
   status == "hurricane"
 )
 
 
 # PIPE OPERATOR (|>)
-# allows the first argument(name of Data Set) to be written outside and continued 
+# allows the first argument(name of Data S   et) to be written outside and continued 
 # by the other arguments. Its similar to nesting in python. The output of one is the
 # input for another. 
 
-storms |>    # from the DS storms...
+hurricanes <- storms |>    # from the DS storms...
   select(!c(lat, long, pressure, ends_with("diameter"))) |>   # ignore columns lat, long.....
   filter(status == "hurricane") |>   # show rows only with 'hurricane' as status.
   arrange(desc(wind), name) |>  # show max wind speed at the top and names in ascending. 
   distinct(name, year, .keep_all = TRUE)  # unique names with year while keeps others rows similar to main DS.
 
+hurricanes
+
+hurricanes |>
+  select(c(year, name, wind)) |>
+  write.csv("hurricanes.csv", row.names = FALSE) 
+
+# strongest hurricane in each given year.
 
 
 
