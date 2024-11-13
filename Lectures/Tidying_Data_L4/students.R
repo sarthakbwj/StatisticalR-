7ca71f4566312bb4bcdc32bbd2068a8a285b7415
sarthakbwj <156067427+sarthakbwj@users.noreply.github.com>
2024-11-13 22:03:59 +0530

    Update students.R

--- Lectures/Tidying_Data_L4/students.R
+++ Lectures/Tidying_Data_L4/students.R
@@ -1,2 +1,10 @@
+library("tidyr")
 students <- read.csv("/Users/sarthakbwj/Documents/GitHub/Statistical_R/Lectures/Tidying_Data_L4/students.csv")
-View(students)
+
+
+pivot_wider(
+  students,
+  id_cols = student, # make first column as 'student'
+  names_from = attribute, # take column names from 'attribute'
+  values_from = value # take rows from 'value'
+)
