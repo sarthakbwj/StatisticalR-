1d9bc33c7b18efb84e442b108433e79462e0df79
sarthakbwj <156067427+sarthakbwj@users.noreply.github.com>
2024-11-16 22:11:03 +0530

    candy

--- Lectures/Visualizing_Data_L5/anita.R
+++ Lectures/Visualizing_Data_L5/anita.R
@@ -1,1 +1,13 @@
 load("/Users/sarthakbwj/Documents/GitHub/Statistical_R/Lectures/Visualizing_Data_L5/anita.RData")
+
+ggplot(
+  anita,
+  aes(x = timestamp, y = wind)) +
+    geom_point() +
+  geom_line() +
+  labs(
+    x = "Data",
+    y = "Wind Speed",
+    title = "Hurricane Anita"
+  ) + 
+  theme_classic()
