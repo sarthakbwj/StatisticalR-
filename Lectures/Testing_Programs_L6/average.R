4b0e3e9bff0544b338d5d97277426bf98f8421b3
sarthakbwj <156067427+sarthakbwj@users.noreply.github.com>
2024-11-17 13:00:04 +0530

    Update average.R

--- Lectures/Testing_Programs_L6/average.R
+++ Lectures/Testing_Programs_L6/average.R
@@ -1,7 +1,26 @@
+# message - to notify the user of a shortcoming in the program.
+# warning - to warn the user of an issue/error.
+# stop - to terminate the program due to an error.
+
 average <- function(x) {
   if(!is.numeric(x)) {
-    warning("'x' must be a numberic vector. Returning NA instead.")
-    return(NA)
+    stop("`x` must be a numberic vector.")
+  }
+  if (any(is.na(x))) {
+      warning("`x` contains one or more NA values.")
+      return(NA)
   }
   sum(x)/length(x)
 }
+
+
+
+
+
+
+
+
+
+
+
+
